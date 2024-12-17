fc4d55eb0ff7f6f1ea972c120709983df92e9eec
LoopinJyoti <162190100+LoopinJyoti@users.noreply.github.com>
2024-12-16 19:10:41 -0500

    Delete Week 6 - Encapsulation & Polymorphism/Lab9_Solution/DataPackage/Lab9Main.java

--- Week 6 - Encapsulation & Polymorphism/Lab9_Solution/DataPackage/Lab9Main.java
+++
@@ -1,8 +0,0 @@
-import DataPackage.DataSnooper;
-
-public class Lab9Main {
-    public static void main(String[] args) {
-        DataSnooper snooper = new DataSnooper();
-        snooper.snoop();
-    }
-}
