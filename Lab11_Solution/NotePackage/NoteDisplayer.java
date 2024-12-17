d6a8187fa32887a8ad6d90c9edd123cbe35a38ec
LoopinJyoti <162190100+LoopinJyoti@users.noreply.github.com>
2024-12-16 19:33:03 -0500

    Delete Lab11_Solution directory

--- Lab11_Solution/NotePackage/NoteDisplayer.java
+++
@@ -1,7 +0,0 @@
-package NotePackage;
-
-public abstract  class  NoteDisplayer {
-
-    public abstract void display(Note note);
-
-}
--- Lab11_Solution/NotePackage/SimpleNoteDsiplay.java
+++
@@ -1,9 +0,0 @@
-package NotePackage;
-
-public class SimpleNoteDsiplay extends NoteDisplayer {
-
-    public void display(Note note)
-    {
-        note.toString();
-    }
-}
